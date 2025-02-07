237506a8b3682c06d9e68de215ba180d5533e1e0
Sven <sven@vioms.com>
2025-02-07 03:08:49 +0100

    Add schedules to timeline

---
+++ src/View/Components/TimelineBadge.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Cachet\View\Components;
+
+use Closure;
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasIcon;
+use Filament\Support\Contracts\HasLabel;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class TimelineBadge extends Component
+{
+    public function __construct(public readonly string $icon, public readonly null|string|array $color = null, public readonly string $label = "")
+    {
+        //
+    }
+
+    public function render(): View|Closure|string
+    {
+        return view('cachet::components.timeline-badge', [
+            'color' => $this->color ?? 'gray',
+            'label' => $this->label,
+            'icon' => $this->icon,
+        ]);
+    }
+}
